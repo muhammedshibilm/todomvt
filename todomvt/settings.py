40741fc693e981d97e545b9c882693f73b261b86
Muhammed Shibil M <95218558+muhammedshibilm@users.noreply.github.com>
2024-05-07 03:56:01 +0530

    initial commit

--- todomvt/settings.py
+++ todomvt/settings.py
@@ -124,12 +124,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'staticfiles_build/'
-STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles_build",
-]
+STATIC_ROOT= BASE_DIR/'static'
+STATIC_URL =  'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+
