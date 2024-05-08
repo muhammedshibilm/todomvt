d431a94fae46a6019c3ac7a268181e1819e1bb73
Muhammed Shibil M <95218558+muhammedshibilm@users.noreply.github.com>
2024-05-08 19:07:53 +0530

    final version

--- todomvt/settings.py
+++ todomvt/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app','now.sh']
 
 
 # Application definition
@@ -126,6 +126,7 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
